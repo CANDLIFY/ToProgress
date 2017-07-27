cb8bf5dca19271f0fe9798cb2e44651006679ba1
tayutaedomo <tayutaedomo@gmail.com>
2017-07-27 13:22:46 +0900

    Chage relative to absolute

--- ToProgress.js
+++ ToProgress.js
@@ -59,7 +59,7 @@
       }
     }
     this.progressBar.setCSS({
-      "position": selector ? "relative" : "fixed",
+      "position": selector ? "absolute" : "fixed",
       "top": "0",
       "left": "0",
       "right": "0",
